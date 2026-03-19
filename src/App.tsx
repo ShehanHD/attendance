44af4667799de0d171b352bf984e09f25746efb4
Don <don.hd@vcloudsystems.com>
2026-03-19 13:54:22 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path='/' element={<Home />} />
         <Route path='/attendance' element={<Attendance />} />
         <Route path='/summary' element={<Summary />} />
